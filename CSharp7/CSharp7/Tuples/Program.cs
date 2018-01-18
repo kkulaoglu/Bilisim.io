6f17fef1daa0c14f25f4bbeb78aa516c9f2b4e89
kkulaoglu <kkulaoglu@gmail.com>
2018-01-18 22:38:07 +0300

    Tuples initialize

--- CSharp7/CSharp7/Tuples/Program.cs
+++ CSharp7/CSharp7/Tuples/Program.cs
@@ -10,6 +10,19 @@ namespace Tuples
     {
         static void Main(string[] args)
         {
+            // Tip tanımıyla gösterimi
+            (double lat, double long, string locationName) tuple = GetCoordinate("istanbul");
+            Console.WriteLine(String.Format("{0} coordinate is lat: {1} - long: {2}", tuple.locationName, tuple.lat, tuple.long));
+            // var anahtar kelimesi ile kullanımı
+            var tupleLoc = GetCoordinate("istanbul");
+            Console.WriteLine(String.Format("{0} coordinate is lat: {1} - long: {2}", tupleLoc.locationName, tupleLoc.lat, tupleLoc.long));
+
+            Console.ReadKey();
+        }
+        private static (double lat, double long, string locationName) GetCoordinate(string locationName)
+        {
+            // https://www.latlong.net/place/istanbul-turkey-2242.html
+            return (41.015137, 28.979530, locationName);
         }
     }
 }
