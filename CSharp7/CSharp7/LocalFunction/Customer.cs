eba1b4d02e0188492bcf74154f50e1ff47b71304
kkulaoglu <kkulaoglu@gmail.com>
2018-01-24 20:15:28 +0300

    .
    .

---
+++ CSharp7/CSharp7/LocalFunction/Customer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalFunction
+{
+    class Customer
+    {
+        public string Name { get; set; }
+        public int Balance { get; set; }
+        public bool Vip
+        {
+            get
+            {
+                return CustomerPriority();
+                bool CustomerPriority()
+                {
+                    return Balance > 1000 ? true : false;
+                }
+            }
+        }
+        public override string ToString()
+        {
+            return String.Format("{0} {1} öncelikli bir müşteridir. ", Name, CustomerPriority(Balance));
+            // local function:
+            string CustomerPriority(int balance)
+            {
+                return balance > 1000 ? "H" : "L";
+            }
+        }
+    }
+}
--- CSharp7/CSharp7/LocalFunction/Program.cs
+++ CSharp7/CSharp7/LocalFunction/Program.cs
@@ -10,6 +10,37 @@ namespace LocalFunction
     {
         static void Main(string[] args)
         {
+            // Local Function 1
+            DisplayCustomerListData();
+
+            // Local Function  2
+            Customer cust = new Customer { Name = "Bilisim.io", Balance = 100000 };
+            Console.WriteLine(cust.ToString());
+
+            // Local Function  get ör3
+            Customer cust3 = new Customer { Name = "Bilisim.io", Balance = 200000 };
+            Console.WriteLine(cust3.Vip);
+
+            Console.ReadKey();
+        }
+        private static void DisplayCustomerListData()
+        {
+            int listSize = 20; // Hatalı kullanım fakat göstermek için
+            List<Customer> PopulateCustomer()
+            {
+                List<Customer> list = new List<Customer>();
+                for (int i = 0; i < listSize; i++)
+                {
+                    list.Add(new Customer { Name = "Customer " + i.ToString(), Balance = 500 * i });
+                }
+                return list;
+            }
+
+            List<Customer> listOfCutomer = PopulateCustomer();
+            foreach (var cust in listOfCutomer)
+            {
+                Console.WriteLine(string.Format(" Customer : {0} - {1}- {2}", cust.Name, cust.Balance, cust.ToString()));
+            }
         }
     }
 }
